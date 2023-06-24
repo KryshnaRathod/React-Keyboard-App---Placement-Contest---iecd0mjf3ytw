4af40086ccd67dc4a2f2b106c565cd1cf5748b26
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-06-24 18:13:52 +0530

    Updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,33 +5,6 @@ const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
 
-  
-   const [previewText, setPreviewText] = useState('');
-  const [quote, setQuote] = useState('');
-
-  useEffect(() => {
-    if (previewText.toLowerCase() === 'forty two') {
-      fetchQuote();
-    } else {
-      setQuote('');
-    }
-  }, [previewText]);
-
-  const fetchQuote = async () => {
-    try {
-      const response = await fetch('https://api.quotable.io/random');
-      const data = await response.json();
-      setQuote(data.content);
-    } catch (error) {
-      console.log('Error fetching quote:', error);
-    }
-  };
-
-  const handleKeyPress = (event) => {
-    const keyPressed = event.target.id.split('-')[1];
-    const newPreviewText = previewText + keyPressed;
-    setPreviewText(newPreviewText);
-  };
 
   return (
     <div className="keyboard">

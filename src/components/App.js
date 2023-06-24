98b1441cd8231a250ee3fd01cf040d372582ab12
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-06-24 17:21:06 +0530

    Updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,42 @@ const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
 
+  const [previewText, setPreviewText] = useState("");
+
+  useEffect(() => {
+    if (previewText.toLowerCase() === "forty two") {
+      fetchQuote();
+    }
+  }, [previewText]);
+
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      // Assuming the API response contains a 'content' property for the quote
+      const quote = data.content;
+      // Update the preview text and set the quote
+      setPreviewText(quote);
+    } catch (error) {
+      console.log("Error fetching quote:", error);
+    }
+  };
+
+  const handleKeyPress = (key) => {
+    const newPreviewText = previewText + key;
+    setPreviewText(newPreviewText);
+  };
+
   return (
     <div className="keyboard">
-      <div className="preview"></div>
+      <div className="preview">{previewText}</div>
       <div>
         {keys.map((key) => (
-          <button key={key} id={key === " " ? `key-space` : `key-${key}`}>
+          <button
+            key={key}
+            id={key === " " ? `key-space` : `key-${key}`}
+            onClick={() => handleKeyPress(key)}
+          >
             {key === " " ? "Space" : key.toUpperCase()}
           </button>
         ))}

efcb0c54b00100f548306ba8e35c90bd8fd04e3f
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-07-18 15:54:47 +0530

    Updated  App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,47 @@
-import "../styles/App.css";
-import React, { useState, useEffect } from "react";
 
-const keys = 'abcdefghijklmnopqrstuvwxyz0123456789 '.split('');
+   
+      import "../styles/App.css";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
-  const [previewText, setPreviewText] = useState('');
+  const [preview, setPreview] = useState('');
   const [quote, setQuote] = useState('');
 
-  useEffect(() => {
-    if (previewText.toLowerCase() === 'forty two') {
-      fetchQuote();
+  const handleClick = (keyValue) => {
+    const updatedPreview = preview + keyValue;
+    setPreview(updatedPreview);
+
+    if (updatedPreview === 'forty two') {
+      axios.get('https://api.quotable.io/random').then((response) => {
+        setQuote(response.data.content);
+      });
     } else {
       setQuote('');
     }
-  }, [previewText]);
-
-  const fetchQuote = async () => {
-    try {
-      const response = await fetch('https://api.quotable.io/random');
-      const data = await response.json();
-      setQuote(data.content);
-    } catch (error) {
-      console.log('Error fetching quote:', error);
-    }
   };
-
-  const handleKeyPress = (event) => {
-    const keyPressed = event.target.id.split('-')[1];
-    const newPreviewText = previewText + keyPressed;
-    setPreviewText(newPreviewText);
+  const renderKeys = () => {
+   
+    return keys.map((keyValue) => (
+      <button key={`key-${keyValue}`} id={`key-${keyValue === ' ' ? 'space' : keyValue}`} onClick={() => handleClick(keyValue)}>
+        {keyValue}
+      </button>
+    ));
   };
 
   return (
-    <div className="keyboard">
-      <div className="preview">{previewText}</div>
-      <div>
-        {keys.map((key) => (
-          <button
-            key={key}
-            id={`key-${key}`}
-            onClick={handleKeyPress}
-          >
-            {key === ' ' ? 'Space' : key.toUpperCase()}
-          </button>
-        ))}
-      </div>
-      {previewText.toLowerCase() === 'forty two' && (
-        <div className="quote">{quote}</div>
-      )}
+    <div className="App">
+      <div className="preview">{preview}</div>
+      {preview === 'forty two' && <div className="quote">{quote}</div>}
+      <div className="keyboard">{renderKeys()}</div>
     </div>
   );
+
 };
 
+
+
 export default App;
- 
-  
 
